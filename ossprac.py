e531c915bcf603fc4f590fe7ff35d011dd7dba93
jaehyukjung <jhdool1594@gmail.com>
2022-09-29 13:21:21 +0900

    2022-09-29 실습과제#4 5

--- ossprac.py
+++ ossprac.py
@@ -3,4 +3,4 @@ student_id = input("학번을 입력하세요 : ")
 major = input("학과을 입력하세요 : ")
 univ = input("학교을 입력하세요 : ")
 
-print(f"\n<출력>\n이름: {name}\n학번: {student_id}\n학과: {major}\n학과: {univ}")
+print(f"\n<출력>\n이름 : {name}\n학번 : {student_id}\n학과 : {major}\n학과 : {univ}")
